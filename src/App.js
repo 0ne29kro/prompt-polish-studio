6788395aaf9c836aaba31ec874bd53ed5d0f549a
0ne29kro <0ne29kro@gmail.com>
2026-03-07 15:02:13 -0600

    remove unused useRef import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Radar, RadarChart, PolarGrid, PolarAngleAxis } from 'recharts';
 
 // Animated score counter
